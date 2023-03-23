83bbc694dff8524d59e9685315c8d77176f8dd05
MuhammadAlhafiz1 <94884867+MuhammadAlhafiz1@users.noreply.github.com>
2023-03-23 17:59:36 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Selamat Datang di Aplikasi Sederhana</Text>
+      <Text style={styles.subtitle}>Ini adalah contoh aplikasi React Native</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
